205b2934e903e238ab052d39ec0c257a449b45c3
Ron Shnapp <ronshnapp@gmail.com>
2022-01-25 13:33:48 +0100

    particle segmentation is working fine

--- segmentation_mod.py
+++ segmentation_mod.py
@@ -20,17 +20,18 @@ class particle_segmentation(object):
     '''a class for segmenting out particles (blobs) for a given image'''
     
     
-    def __init__(self, image, sigma=None, threshold=10, mask=1.0,
-                 local_winsize=None, local_threshold=None):
+    def __init__(self, image, sigma=1.0, threshold=10, mask=1.0,
+                 min_xsize=None, max_xsize=None,
+                 min_ysize=None, max_ysize=None,
+                 min_area=None, max_area=None):
         
         self.im = image
         self.sigma = sigma
         self.th = threshold
-        self.lws = local_winsize
-        self.lth = local_threshold
-        self.bin_im = self.get_binary_image() 
-        self.blob_pixels = self.blob_labeling(self.bin_im)
-    
+        self.mask = mask
+        self.bbox_limits = (min_xsize, max_xsize, min_ysize, max_ysize)
+        self.area_limits = (min_area, max_area)
+        
         
     def get_binary_image(self):
         '''Will mark pixels in the image as background and foreground 
@@ -44,9 +45,8 @@ class particle_segmentation(object):
             blured = self.im
             
         global_filt = blured>self.th
-
             
-        bin_image = 1.0 * global_filt
+        bin_image = 1.0 * global_filt * self.mask
         return bin_image 
     
 
@@ -87,27 +87,93 @@ class particle_segmentation(object):
     
     
     
-    def local_threshold(self, image):
-        '''applies a local threshold to see if each pixel is higher
-        than some threshold above the local average.'''
+    def get_blobs(self):
+        '''Returns a list of particle centers, their box size, and area
         
-        if self.lws is None:
-            raise ValueError('local window size not defined')
+        The center is the weighted mean of the blob coordinates using
+        the brightness as weights.
+        The box size is the larger lengths than bound the blob in the
+        x and y directions.
+        The area is the number of pixels belonging to the blob
+        
+        returns - blobs: a nested list of [ [(center), (box size), area], ... ]
+        '''
+        
+        self.bin_im = self.get_binary_image() 
+        self.blob_pixels = self.blob_labeling(self.bin_im)
+        
+        blobs = []
+        
+        for i in range(len(self.blob_pixels)):
             
-        if self.lth is None:
-            raise ValueError('local threshold size not defined')
+            X = 0.0
+            Y = 0.0
+            tot = 0.0
+            
+            for x,y in self.blob_pixels[i]:
+                
+                if tot == 0.0:
+                    xmin, xmax = x, x
+                    ymin, ymax = y, y
+                else:
+                    if x<xmin: xmin=x
+                    if x>xmax: xmax=x
+                    if y<ymin: ymin=y
+                    if y>ymax: ymax=y
+                
+                bxy = self.im[x,y]
+                X += x*bxy
+                Y += y*bxy
+                tot += bxy
+            center = [X/tot, Y/tot]
+            box_size = [xmax-xmin+1, ymax-ymin+1]
+            area = len(self.blob_pixels[i])
+            blobs.append( [center, box_size, area])
+            
+        self.blobs = blobs
         
-        wi = int(self.lws/2)
-        result = zeros(self.im.shape)
-        for i in range(wi,self.im.shape[0]-wi):
-            for j in range(wi,self.im.shape[1]-wi):
-                if image[i,j]>0:
-                    local_av = mean(image[i-wi:i+wi+1,j-wi:j+wi+1])
-                    if image[i,j] > local_av + self.lth:
-                        result[i,j] = 1
-        return result
-
-
+        
+    def apply_blobs_size_filter(self):
+        '''Will filter the list of blobs accoring to their bounding box size 
+        and their area.'''
+        
+        if self.bbox_limits[0] is not None:
+            fltr = lambda b: b[1][0] > self.bbox_limits[0]
+            self.blobs = list(filter(fltr, self.blobs))
+        
+        if self.bbox_limits[1] is not None:
+            fltr = lambda b: b[1][0] < self.bbox_limits[1]
+            self.blobs = list(filter(fltr, self.blobs))
+        
+        if self.bbox_limits[2] is not None:
+            fltr = lambda b: b[1][1] > self.bbox_limits[2]
+            self.blobs = list(filter(fltr, self.blobs))
+        
+        if self.bbox_limits[3] is not None:
+            fltr = lambda b: b[1][1] < self.bbox_limits[3]
+            self.blobs = list(filter(fltr, self.blobs))
+            
+        if self.area_limits[0] is not None:
+            fltr = lambda b: b[2] > self.area_limits[0]
+        
+        if self.area_limits[1] is not None:
+            fltr = lambda b: b[2] < self.area_limits[1]
+            
+            
+    def plot_blobs(self):
+        import matplotlib.pyplot as plt
+        fig, ax = plt.subplots()
+        ax.imshow(self.im, vmin=self.th/3, vmax=3*self.th)
+        
+        for blb in self.blobs:
+            
+            ax.errorbar( [blb[0][1]], [blb[0][0]], 
+                        xerr=blb[1][1]/2, yerr=blb[1][0]/2,
+                        fmt='xr', lw=0.7, capsize=2)
+        
+    
+    
+    
         
     
     
