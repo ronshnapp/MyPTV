34e139ba35c089a1c61bbf26c18f3728053bc6ab
Ron Shnapp <ronshnapp@gmail.com>
2022-03-22 12:08:38 +0100

    added a particle segmentation test

---
+++ tests/test_segmentation_mod.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  7 18:02:07 2018
+
+@author: ron
+
+A test function for the segmentation module
+
+"""
+
+
+from myptv.segmentation_mod import loop_segmentation, particle_segmentation
+
+
+
+def test_segmentation():
+    '''
+    A test for the segmentation module, trying to segment blobs from a 
+    synthetic noisy image.
+    '''
+    dirname = './tests/segmentation_test_files'
+    
+    segmentation = loop_segmentation(dirname, threshold=50, sigma=1.0)
+    segmentation.segment_folder_images()
+    blobs = segmentation.blobs
+    test_blob_number = len(blobs)==8
+    assert test_blob_number
+
+
