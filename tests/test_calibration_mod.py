a75b1784362344e139b9be2f9c446caf556b9a7c
Ron Shnapp <ronshnapp@gmail.com>
2022-03-22 10:00:28 +0100

    included the calibration point file in the user manual

---
+++ tests/test_calibration_mod.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  7 18:02:07 2018
+
+@author: ron
+
+A test function for the imaging module
+
+"""
+
+from myptv.imaging_mod import camera
+from myptv.calibrate_mod import calibrate 
+from numpy import array
+from math import pi
+
+
+def test_calibrate():
+    '''
+    A test for the calibrate class. We attempt to calibrate a 
+    synthetic camera.
+    '''
+    
+    assert a == 0.1 and b == 0.1, c == 0.1
+
